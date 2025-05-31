8929f0edb5b836cedcb031d8953b8d73f514904c
FerG0373 <fer.dg.sc@gmail.com>
2025-05-30 23:55:00 -0300

    Corrección nombre de archivo en las pantallas.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:alnabekapp/screens/src1_portada.dart';
-import 'package:alnabekapp/screens/src2_login.dart';
-import 'package:alnabekapp/screens/src3_home.dart';
-import 'package:alnabekapp/screens/src4_entrada.dart';
-import 'package:alnabekapp/screens/src5_shawarma.dart';
+import 'package:alnabekapp/screens/scr1_portada.dart';
+import 'package:alnabekapp/screens/scr2_login.dart';
+import 'package:alnabekapp/screens/scr3_home.dart';
+import 'package:alnabekapp/screens/scr4_entrada.dart';
+import 'package:alnabekapp/screens/scr5_shawarma.dart';
 
 Map <String, Widget Function(BuildContext)> rutasApp = {
   '/': (context) => const PortadaScreen(),
