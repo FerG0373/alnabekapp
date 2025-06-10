e9b629c20472ea9b553d1ce85e48b068e98135a3
FerG0373 <fer.dg.sc@gmail.com>
2025-06-10 15:57:07 -0300

    factory en clase Shawarma

--- lib/models/shawarma.dart
+++ lib/models/shawarma.dart
@@ -1,24 +1,38 @@
-// class Shawarma {
-//   final int _id;
-//   final String _nombre;
-//   final String? _descripcion;
-//   final double _precio;
-//   final String _tipo;
+class Shawarma {
+  final int _id;
+  final String _nombre;
+  final String? _descripcion;
+  final double? _precio;
+  final String _tipo;
 
-//   // Constructor.
-//   Shawarma({
-//     required this.id,
-//     required this.nombre,
-//     this.descripcion,
-//     this.precio,
-//     required this.tipo,
-//   });
+  // Constructor.
+  Shawarma({
+    required int id,
+    required String nombre,
+    String? descripcion,
+    double? precio,
+    required String tipo,
+  }) : _id = id,
+       _nombre = nombre,
+       _descripcion = descripcion,
+       _precio = precio,
+       _tipo = tipo;
 
-//   // Getters.
-//   int get id => _id;
-//   String get nombre => _nombre;
-//   String? get descripcion => _descripcion;
-//   double get precio => _precio;
-//   String get imagen => _tipo;
-  
-// }
+  // Getters.
+  int get id => _id;
+  String get nombre => _nombre;
+  String? get descripcion => _descripcion;
+  double? get precio => _precio;
+  String get imagen => _tipo;
+
+  // Convierte JSON a Map.
+  factory Shawarma.fromJson(Map<String, dynamic> json) {
+    return Shawarma(
+      id: json['id_comida'] ?? 0,
+      nombre: json['nombre_comida'],
+      descripcion: json['descripcion_comida'] ?? '',
+      precio: json['precio_comida'] ?? '',
+      tipo: json['tipo_comida'],
+    );
+  }
+}
