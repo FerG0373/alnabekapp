c72755672607a7a423469cb354295c5e4c3ee3b2
FerG0373 <fer.dg.sc@gmail.com>
2025-06-10 08:58:29 -0300

    Empezando clase Shawarma

--- lib/models/shawarma.dart
+++ lib/models/shawarma.dart
@@ -1,16 +1,24 @@
-class Shawarma {
-  final String _nombre;
-  final String _descripcion;
-  final double _precio;
-  final String _imagen;
+// class Shawarma {
+//   final int _id;
+//   final String _nombre;
+//   final String? _descripcion;
+//   final double _precio;
+//   final String _tipo;
 
-  // Constructor.
-  Shawarma(this._nombre, this._descripcion, this._precio, this._imagen);
+//   // Constructor.
+//   Shawarma({
+//     required this.id,
+//     required this.nombre,
+//     this.descripcion,
+//     this.precio,
+//     required this.tipo,
+//   });
 
-  // Getters.
-  String get nombre => _nombre;
-  String get descripcion => _descripcion;
-  double get precio => _precio;
-  String get imagen => _imagen;
+//   // Getters.
+//   int get id => _id;
+//   String get nombre => _nombre;
+//   String? get descripcion => _descripcion;
+//   double get precio => _precio;
+//   String get imagen => _tipo;
   
-}
+// }
