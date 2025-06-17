45a734e156643b8ca02db684e6c79454b5789c28
FerG0373 <fer.dg.sc@gmail.com>
2025-06-17 09:06:49 -0300

    Clase ShawarmaService; corrección en el modelo de shawarmas

--- lib/models/shawarma.dart
+++ lib/models/shawarma.dart
@@ -2,7 +2,7 @@ class Shawarma {
   final int _id;
   final String _nombre;
   final String? _descripcion;
-  final double? _precio;
+  final double _precio;
   final String _tipo;
 
   // Constructor.
@@ -10,7 +10,7 @@ class Shawarma {
     required int id,
     required String nombre,
     String? descripcion,
-    double? precio,
+    required double precio,
     required String tipo,
   }) : _id = id,
        _nombre = nombre,
@@ -22,7 +22,7 @@ class Shawarma {
   int get id => _id;
   String get nombre => _nombre;
   String? get descripcion => _descripcion;
-  double? get precio => _precio;
+  double get precio => _precio;
   String get imagen => _tipo;
 
   // Convierte JSON a Map.
@@ -31,7 +31,7 @@ class Shawarma {
       id: json['id_comida'] ?? 0,
       nombre: json['nombre_comida'],
       descripcion: json['descripcion_comida'] ?? '',
-      precio: json['precio_comida'] ?? '',
+      precio: (json['precio_comida'] as num).toDouble(),
       tipo: json['tipo_comida'],
     );
   }
---
+++ lib/services/shawarma_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:alnabekapp/models/shawarma.dart';
+import 'package:http/http.dart' as http;
+
+class ShawarmaService {
+  final String baseUrl = "https://api.awspaints.com.ar/index.php";
+
+  Future<List<Shawarma>> getShawarmas() async {
+    final response = await http.get(Uri.parse('$baseUrl/shawarmaComidas/0'));
+
+    if(response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+
+      if(jsonResponse is List) {
+        List<Shawarma> shawarmas = jsonResponse.map((data) => Shawarma.fromJson(Map<String, dynamic>.from(data))).toList();
+
+        shawarmas.sort((a, b) => b.id.compareTo(a.id));
+        return shawarmas;
+      } else {
+        throw Exception('Error: Formato de datos inesperado');
+      }
+    } else {
+      throw Exception('Error al cargar los shawarmas: ${response.statusCode}');
+    }
+  }
+} 
